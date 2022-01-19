b1bb0be72a50fbe6c00a0c0277227204ad4ddc70
ronak111 <ronak.sheth@sheridancollege.ca>
2022-01-19 10:16:33 -0500

    added one more System.out.println  line at line 29

--- src/student_win22_wed/Student_win22_wed.java
+++ src/student_win22_wed/Student_win22_wed.java
@@ -26,6 +26,7 @@ public class Student_win22_wed {
         }*/
        Scanner sc =new Scanner(System.in);
        System.out.print("Please enter a word");
+       System.out.print("This line is added");
        String word = sc.nextLine();
        
        char[] myLetters = new char[word.length()];
