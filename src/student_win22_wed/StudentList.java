483faf3d72fdba3967de3d84cd6293a926161b74
ronak111 <ronak.sheth@sheridancollege.ca>
2022-01-19 10:07:28 -0500

    Array size changed from 3 to 4

--- src/student_win22_wed/StudentList.java
+++ src/student_win22_wed/StudentList.java
@@ -3,7 +3,7 @@ package student_win22_wed;
 
 public class StudentList {
       public static void main(String[] args) {
-          Student[] studentList = new Student[3];
+          Student[] studentList = new Student[4];
           studentList[0] = new Student("s1","Ronak Sheth");
           studentList[1] = new Student("s2","John Doe");
           studentList[2] = new Student("s3","abc");
