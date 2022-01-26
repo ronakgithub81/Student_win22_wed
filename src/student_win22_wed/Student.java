69300808445a5dcc29a11010d9de71edea29cf9b
ronak sheth <ronak.sheth@sheridancollege.ca>
2022-01-26 08:50:18 -0500

    added new variable address

--- src/student_win22_wed/Student.java
+++ src/student_win22_wed/Student.java
@@ -9,10 +9,19 @@ package student_win22_wed;
 public class Student {
     private String studentId;
     private String studentName;
+    private String address;
     
     public Student(String studentId,String studentName){
         this.studentId=studentId;
         this.studentName=studentName;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
     }
 
     public String getStudentId() {
